b8a442bcd435cb5cfa83b18eb246f75ef55a2b0c
Usagi Mero <usagimero@gmail.com>
2023-05-28 20:14:51 -0700

    Work on movement

---
+++ Editor v4.0/Assets/Scenes/ED3/Scene Switch Events/A1_to_S1.cs
@@ -0,0 +1,103 @@
+using Unity.VisualScripting; using Assets.Event_Scripts;
+public class A1_to_S1 : EventController {
+	void Start() { Load(); }
+	public void Load() {
+		SerializationData data = new SerializationData(
+		"{" +
+			"  \"_pipes\": [" +
+			"    {" +
+			"      \"command\": {" +
+			"        \"_targetSceneName\": \"S1\"," +
+			"        \"_targetPlayerPosition\": {" +
+			"          \"x\": 0.0," +
+			"          \"y\": -0.1870002," +
+			"          \"z\": 0.0" +
+			"        }," +
+			"        \"next\": null," +
+			"        \"controller\": null," +
+			"        \"$type\": \"Assets.Event_Editor.Event_Scripts.Commands.SceneSwitchCommand\"" +
+			"      }," +
+			"      \"$type\": \"Assets.Event_Editor.Event_Scripts.CommandPipeSystem\"," +
+			"      \"$id\": \"2\"" +
+			"    }," +
+			"    {" +
+			"      \"conditions\": [" +
+			"        {" +
+			"          \"_gameObjectFrom\": null," +
+			"          \"_gameObjectTo\": null," +
+			"          \"_triggerLimit\": 1.0," +
+			"          \"_inside\": true," +
+			"          \"next\": {" +
+			"            \"conditions\": [" +
+			"              {" +
+			"                \"_awaitedKey\": \"Z\"," +
+			"                \"_pressed\": true," +
+			"                \"next\": {" +
+			"                  \"conditions\": [" +
+			"                    {" +
+			"                      \"_gameObjectFrom\": null," +
+			"                      \"_gameObjectTo\": null," +
+			"                      \"_triggerLimit\": 1.0," +
+			"                      \"_inside\": true," +
+			"                      \"next\": {" +
+			"                        \"$ref\": \"2\"" +
+			"                      }," +
+			"                      \"controller\": null," +
+			"                      \"$type\": \"Assets.Event_Scripts.Conditions.ProximityCondition\"" +
+			"                    }," +
+			"                    {" +
+			"                      \"_gameObjectFrom\": null," +
+			"                      \"_gameObjectTo\": null," +
+			"                      \"_triggerLimit\": 1.0," +
+			"                      \"_inside\": false," +
+			"                      \"next\": {" +
+			"                        \"$ref\": \"4\"" +
+			"                      }," +
+			"                      \"controller\": null," +
+			"                      \"$type\": \"Assets.Event_Scripts.Conditions.ProximityCondition\"" +
+			"                    }" +
+			"                  ]," +
+			"                  \"$type\": \"Assets.Event_Editor.Event_Scripts.ConditionPipeSystem\"," +
+			"                  \"$id\": \"10\"" +
+			"                }," +
+			"                \"controller\": null," +
+			"                \"$type\": \"Assets.Event_Scripts.Conditions.InputCondition\"" +
+			"              }" +
+			"            ]," +
+			"            \"$type\": \"Assets.Event_Editor.Event_Scripts.ConditionPipeSystem\"," +
+			"            \"$id\": \"7\"" +
+			"          }," +
+			"          \"controller\": null," +
+			"          \"$type\": \"Assets.Event_Scripts.Conditions.ProximityCondition\"," +
+			"          \"$id\": \"6\"" +
+			"        }" +
+			"      ]," +
+			"      \"$type\": \"Assets.Event_Editor.Event_Scripts.ConditionPipeSystem\"," +
+			"      \"$id\": \"4\"" +
+			"    }," +
+			"    {" +
+			"      \"$ref\": \"10\"" +
+			"    }," +
+			"    {" +
+			"      \"$ref\": \"7\"" +
+			"    }," +
+			"    {" +
+			"      \"conditions\": [" +
+			"        {" +
+			"          \"$ref\": \"6\"" +
+			"        }" +
+			"      ]," +
+			"      \"$type\": \"Assets.Event_Editor.Event_Scripts.ConditionPipeSystem\"," +
+			"      \"$id\": \"14\"" +
+			"    }" +
+			"  ]," +
+			"  \"_root\": {" +
+			"    \"$ref\": \"14\"" +
+			"  }," +
+			"  \"$type\": \"Assets.Event_Scripts.EventPipeWrapper\"" +
+			"}");
+		EventPipeWrapper wrapper = (EventPipeWrapper)data.Deserialize();
+		rootPipe = wrapper._root;
+		rootPipe.PropogateController(this);
+	}
+}
