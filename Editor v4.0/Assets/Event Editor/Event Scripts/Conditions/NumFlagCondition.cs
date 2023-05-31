d09004442b81e5b249950897d18e5a2f18cfc31d
“Usagi” <42399857+UsagiMero@users.noreply.github.com>
2023-05-30 19:46:20 -0700

    Work on artifacts

---
+++ Editor v4.0/Assets/Event Editor/Event Scripts/Conditions/NumFlagCondition.cs
@@ -0,0 +1,61 @@
+﻿using Assets.Event_Scripts.Event_Commands;
+using EECore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Assets.Event_Editor.Event_Scripts.Conditions
+{
+    public class NumFlagCondition : ConditionNode
+    {
+        [Serialize]
+        public string _flag;
+
+        [Serialize]
+        public string _check;
+
+        [Serialize]
+        public int _value;
+
+        public NumFlagCondition(string flag, string check, int value)
+        {
+            _flag = flag;
+            _check = check;
+            _value = value;
+        }
+
+        internal override bool IsMet()
+        {
+            if (!GameStateManager.flags.ContainsKey(_flag))
+            {
+                return false;
+            }
+
+            int lhs = _value;
+            int rhs = GameStateManager.flags[_flag];
+
+            //==,!=,<,>,>=,<=,
+            switch (_check)
+            {
+                case "==":
+                    return lhs == rhs;
+                case "!=":
+                    return lhs != rhs;
+                case "<":
+                    return lhs < rhs;
+                case ">":
+                    return lhs > rhs;
+                case ">=":
+                    return lhs >= rhs;
+                case "<=":
+                    return lhs <= rhs;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Editor v4.0/Assets/Scenes/ED3/Artifact Events/artifact_2_collect.cs
@@ -0,0 +1,220 @@
+using Unity.VisualScripting; using Assets.Event_Scripts;
+public class artifact_2_collect : EventController {
+	void Start() { Load(); }
+	public void Load() {
+		SerializationData data = new SerializationData(
+		"{" +
+			"  \"_pipes\": [" +
+			"    {" +
+			"      \"command\": {" +
+			"        \"_name\": \"Artifact 2\"," +
+			"        \"_position\": {" +
+			"          \"x\": -100.0," +
+			"          \"y\": -100.0," +
+			"          \"z\": -100.0" +
+			"        }," +
+			"        \"next\": null," +
+			"        \"controller\": null," +
+			"        \"$type\": \"Assets.Event_Editor.Event_Scripts.Commands.ControlPrefabCommand\"" +
+			"      }," +
+			"      \"$type\": \"Assets.Event_Editor.Event_Scripts.CommandPipeSystem\"," +
+			"      \"$id\": \"2\"" +
+			"    }," +
+			"    {" +
+			"      \"command\": {" +
+			"        \"next\": {" +
+			"          \"$ref\": \"2\"" +
+			"        }," +
+			"        \"controller\": null," +
+			"        \"$type\": \"Assets.Event_Editor.Event_Scripts.Commands.HideTextCommand\"" +
+			"      }," +
+			"      \"$type\": \"Assets.Event_Editor.Event_Scripts.CommandPipeSystem\"," +
+			"      \"$id\": \"4\"" +
+			"    }," +
+			"    {" +
+			"      \"command\": {" +
+			"        \"_flag\": \"artifact 2\"," +
+			"        \"_value\": 1," +
+			"        \"next\": {" +
+			"          \"$ref\": \"4\"" +
+			"        }," +
+			"        \"controller\": null," +
+			"        \"$type\": \"Assets.Event_Editor.Event_Scripts.Commands.SetFlagCommand\"" +
+			"      }," +
+			"      \"$type\": \"Assets.Event_Editor.Event_Scripts.CommandPipeSystem\"," +
+			"      \"$id\": \"6\"" +
+			"    }," +
+			"    {" +
+			"      \"conditions\": [" +
+			"        {" +
+			"          \"_awaitedKey\": \"Z\"," +
+			"          \"_pressed\": true," +
+			"          \"next\": {" +
+			"            \"$ref\": \"6\"" +
+			"          }," +
+			"          \"controller\": null," +
+			"          \"$type\": \"Assets.Event_Scripts.Conditions.InputCondition\"" +
+			"        }" +
+			"      ]," +
+			"      \"$type\": \"Assets.Event_Editor.Event_Scripts.ConditionPipeSystem\"," +
+			"      \"$id\": \"8\"" +
+			"    }," +
+			"    {" +
+			"      \"command\": {" +
+			"        \"_name\": \"\"," +
+			"        \"_portrait\": null," +
+			"        \"_positionLeft\": true," +
+			"        \"_text\": \"You dust off the artifact and put it in your pouch.\"," +
+			"        \"next\": {" +
+			"          \"$ref\": \"8\"" +
+			"        }," +
+			"        \"controller\": null," +
+			"        \"$type\": \"Assets.Event_Editor.Event_Scripts.Commands.ShowTextCommand\"" +
+			"      }," +
+			"      \"$type\": \"Assets.Event_Editor.Event_Scripts.CommandPipeSystem\"," +
+			"      \"$id\": \"11\"" +
+			"    }," +
+			"    {" +
+			"      \"conditions\": [" +
+			"        {" +
+			"          \"_awaitedKey\": \"Z\"," +
+			"          \"_pressed\": true," +
+			"          \"next\": {" +
+			"            \"$ref\": \"11\"" +
+			"          }," +
+			"          \"controller\": null," +
+			"          \"$type\": \"Assets.Event_Scripts.Conditions.InputCondition\"" +
+			"        }" +
+			"      ]," +
+			"      \"$type\": \"Assets.Event_Editor.Event_Scripts.ConditionPipeSystem\"," +
+			"      \"$id\": \"13\"" +
+			"    }," +
+			"    {" +
+			"      \"command\": {" +
+			"        \"_name\": \"\"," +
+			"        \"_portrait\": null," +
+			"        \"_positionLeft\": true," +
+			"        \"_text\": \"An artifact sitting neatly in a dustry corner of the room.\"," +
+			"        \"next\": {" +
+			"          \"$ref\": \"13\"" +
+			"        }," +
+			"        \"controller\": null," +
+			"        \"$type\": \"Assets.Event_Editor.Event_Scripts.Commands.ShowTextCommand\"" +
+			"      }," +
+			"      \"$type\": \"Assets.Event_Editor.Event_Scripts.CommandPipeSystem\"," +
+			"      \"$id\": \"16\"" +
+			"    }," +
+			"    {" +
+			"      \"command\": {" +
+			"        \"_showFirst\": false," +
+			"        \"_textFirst\": \"option 1 text\"," +
+			"        \"_showSecond\": false," +
+			"        \"_textSecond\": \"option 2 text\"," +
+			"        \"_showThird\": false," +
+			"        \"_textThird\": \"option 3 text\"," +
+			"        \"next\": {" +
+			"          \"$ref\": \"16\"" +
+			"        }," +
+			"        \"controller\": null," +
+			"        \"$type\": \"Assets.Event_Editor.Event_Scripts.Commands.ShowOptionCommand\"" +
+			"      }," +
+			"      \"$type\": \"Assets.Event_Editor.Event_Scripts.CommandPipeSystem\"," +
+			"      \"$id\": \"18\"" +
+			"    }," +
+			"    {" +
+			"      \"conditions\": [" +
+			"        {" +
+			"          \"_gameObjectFrom\": null," +
+			"          \"_gameObjectTo\": null," +
+			"          \"_triggerLimit\": 1.0," +
+			"          \"_inside\": true," +
+			"          \"next\": {" +
+			"            \"conditions\": [" +
+			"              {" +
+			"                \"_awaitedKey\": \"Z\"," +
+			"                \"_pressed\": true," +
+			"                \"next\": {" +
+			"                  \"conditions\": [" +
+			"                    {" +
+			"                      \"_gameObjectFrom\": null," +
+			"                      \"_gameObjectTo\": null," +
+			"                      \"_triggerLimit\": 1.0," +
+			"                      \"_inside\": true," +
+			"                      \"next\": {" +
+			"                        \"$ref\": \"18\"" +
+			"                      }," +
+			"                      \"controller\": null," +
+			"                      \"$type\": \"Assets.Event_Scripts.Conditions.ProximityCondition\"" +
+			"                    }," +
+			"                    {" +
+			"                      \"_gameObjectFrom\": null," +
+			"                      \"_gameObjectTo\": null," +
+			"                      \"_triggerLimit\": 1.0," +
+			"                      \"_inside\": false," +
+			"                      \"next\": {" +
+			"                        \"$ref\": \"20\"" +
+			"                      }," +
+			"                      \"controller\": null," +
+			"                      \"$type\": \"Assets.Event_Scripts.Conditions.ProximityCondition\"" +
+			"                    }" +
+			"                  ]," +
+			"                  \"$type\": \"Assets.Event_Editor.Event_Scripts.ConditionPipeSystem\"," +
+			"                  \"$id\": \"26\"" +
+			"                }," +
+			"                \"controller\": null," +
+			"                \"$type\": \"Assets.Event_Scripts.Conditions.InputCondition\"" +
+			"              }" +
+			"            ]," +
+			"            \"$type\": \"Assets.Event_Editor.Event_Scripts.ConditionPipeSystem\"," +
+			"            \"$id\": \"23\"" +
+			"          }," +
+			"          \"controller\": null," +
+			"          \"$type\": \"Assets.Event_Scripts.Conditions.ProximityCondition\"," +
+			"          \"$id\": \"22\"" +
+			"        }" +
+			"      ]," +
+			"      \"$type\": \"Assets.Event_Editor.Event_Scripts.ConditionPipeSystem\"," +
+			"      \"$id\": \"20\"" +
+			"    }," +
+			"    {" +
+			"      \"$ref\": \"26\"" +
+			"    }," +
+			"    {" +
+			"      \"$ref\": \"23\"" +
+			"    }," +
+			"    {" +
+			"      \"conditions\": [" +
+			"        {" +
+			"          \"$ref\": \"22\"" +
+			"        }" +
+			"      ]," +
+			"      \"$type\": \"Assets.Event_Editor.Event_Scripts.ConditionPipeSystem\"," +
+			"      \"$id\": \"30\"" +
+			"    }," +
+			"    {" +
+			"      \"conditions\": [" +
+			"        {" +
+			"          \"_flag\": \"artifact 2\"," +
+			"          \"_check\": \"==\"," +
+			"          \"_value\": 0," +
+			"          \"next\": {" +
+			"            \"$ref\": \"30\"" +
+			"          }," +
+			"          \"controller\": null," +
+			"          \"$type\": \"Assets.Event_Editor.Event_Scripts.Conditions.NumFlagCondition\"" +
+			"        }" +
+			"      ]," +
+			"      \"$type\": \"Assets.Event_Editor.Event_Scripts.ConditionPipeSystem\"," +
+			"      \"$id\": \"32\"" +
+			"    }" +
+			"  ]," +
+			"  \"_root\": {" +
+			"    \"$ref\": \"32\"" +
+			"  }," +
+			"  \"$type\": \"Assets.Event_Scripts.EventPipeWrapper\"" +
+			"}");
+		EventPipeWrapper wrapper = (EventPipeWrapper)data.Deserialize();
+		rootPipe = wrapper._root;
+		rootPipe.PropogateController(this);
+	}
+}
