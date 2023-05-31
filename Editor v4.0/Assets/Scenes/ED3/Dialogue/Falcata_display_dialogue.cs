c0ea1023bd91393cf4932f0e4f3cdd9b609abb28
Ammanuel Beyene <beyenea1@spu.edu>
2023-05-30 22:06:18 -0700

    Carthage sword dialogue

---
+++ Editor v4.0/Assets/Scenes/ED3/Dialogue/Falcata_display_dialogue.cs
@@ -0,0 +1,144 @@
+using Unity.VisualScripting; using Assets.Event_Scripts;
+public class Falcata_display_dialogue : EventController {
+	void Start() { Load(); }
+	public void Load() {
+		SerializationData data = new SerializationData(
+		"{" +
+			"  \"_pipes\": [" +
+			"    {" +
+			"      \"command\": {" +
+			"        \"next\": null," +
+			"        \"controller\": null," +
+			"        \"$type\": \"Assets.Event_Editor.Event_Scripts.Commands.HideTextCommand\"" +
+			"      }," +
+			"      \"$type\": \"Assets.Event_Editor.Event_Scripts.CommandPipeSystem\"," +
+			"      \"$id\": \"2\"" +
+			"    }," +
+			"    {" +
+			"      \"conditions\": [" +
+			"        {" +
+			"          \"_awaitedKey\": \"Z\"," +
+			"          \"_pressed\": true," +
+			"          \"next\": {" +
+			"            \"$ref\": \"2\"" +
+			"          }," +
+			"          \"controller\": null," +
+			"          \"$type\": \"Assets.Event_Scripts.Conditions.InputCondition\"" +
+			"        }" +
+			"      ]," +
+			"      \"$type\": \"Assets.Event_Editor.Event_Scripts.ConditionPipeSystem\"," +
+			"      \"$id\": \"4\"" +
+			"    }," +
+			"    {" +
+			"      \"command\": {" +
+			"        \"_showFirst\": false," +
+			"        \"_textFirst\": \"option 1 text\"," +
+			"        \"_showSecond\": false," +
+			"        \"_textSecond\": \"option 2 text\"," +
+			"        \"_showThird\": false," +
+			"        \"_textThird\": \"option 3 text\"," +
+			"        \"next\": {" +
+			"          \"$ref\": \"4\"" +
+			"        }," +
+			"        \"controller\": null," +
+			"        \"$type\": \"Assets.Event_Editor.Event_Scripts.Commands.ShowOptionCommand\"" +
+			"      }," +
+			"      \"$type\": \"Assets.Event_Editor.Event_Scripts.CommandPipeSystem\"," +
+			"      \"$id\": \"7\"" +
+			"    }," +
+			"    {" +
+			"      \"command\": {" +
+			"        \"_name\": \"\"," +
+			"        \"_portrait\": null," +
+			"        \"_positionLeft\": true," +
+			"        \"_text\": \" The falcata is a famous sword that the ancient general Hannibal equipped his Carthaginian troops with during the second Punic war. Although the origin of the falcata is not known, it is suspected to have originated in Spain as a descendant of the famous Greek sword, kopis. It is made of steel and had great benefits during combat.  \"," +
+			"        \"next\": {" +
+			"          \"$ref\": \"7\"" +
+			"        }," +
+			"        \"controller\": null," +
+			"        \"$type\": \"Assets.Event_Editor.Event_Scripts.Commands.ShowTextCommand\"" +
+			"      }," +
+			"      \"$type\": \"Assets.Event_Editor.Event_Scripts.CommandPipeSystem\"," +
+			"      \"$id\": \"9\"" +
+			"    }," +
+			"    {" +
+			"      \"conditions\": [" +
+			"        {" +
+			"          \"_gameObjectFrom\": null," +
+			"          \"_gameObjectTo\": null," +
+			"          \"_triggerLimit\": 2.0," +
+			"          \"_inside\": true," +
+			"          \"next\": {" +
+			"            \"conditions\": [" +
+			"              {" +
+			"                \"_awaitedKey\": \"Z\"," +
+			"                \"_pressed\": true," +
+			"                \"next\": {" +
+			"                  \"conditions\": [" +
+			"                    {" +
+			"                      \"_gameObjectFrom\": null," +
+			"                      \"_gameObjectTo\": null," +
+			"                      \"_triggerLimit\": 2.0," +
+			"                      \"_inside\": true," +
+			"                      \"next\": {" +
+			"                        \"$ref\": \"9\"" +
+			"                      }," +
+			"                      \"controller\": null," +
+			"                      \"$type\": \"Assets.Event_Scripts.Conditions.ProximityCondition\"" +
+			"                    }," +
+			"                    {" +
+			"                      \"_gameObjectFrom\": null," +
+			"                      \"_gameObjectTo\": null," +
+			"                      \"_triggerLimit\": 2.0," +
+			"                      \"_inside\": false," +
+			"                      \"next\": {" +
+			"                        \"$ref\": \"11\"" +
+			"                      }," +
+			"                      \"controller\": null," +
+			"                      \"$type\": \"Assets.Event_Scripts.Conditions.ProximityCondition\"" +
+			"                    }" +
+			"                  ]," +
+			"                  \"$type\": \"Assets.Event_Editor.Event_Scripts.ConditionPipeSystem\"," +
+			"                  \"$id\": \"17\"" +
+			"                }," +
+			"                \"controller\": null," +
+			"                \"$type\": \"Assets.Event_Scripts.Conditions.InputCondition\"" +
+			"              }" +
+			"            ]," +
+			"            \"$type\": \"Assets.Event_Editor.Event_Scripts.ConditionPipeSystem\"," +
+			"            \"$id\": \"14\"" +
+			"          }," +
+			"          \"controller\": null," +
+			"          \"$type\": \"Assets.Event_Scripts.Conditions.ProximityCondition\"," +
+			"          \"$id\": \"13\"" +
+			"        }" +
+			"      ]," +
+			"      \"$type\": \"Assets.Event_Editor.Event_Scripts.ConditionPipeSystem\"," +
+			"      \"$id\": \"11\"" +
+			"    }," +
+			"    {" +
+			"      \"$ref\": \"17\"" +
+			"    }," +
+			"    {" +
+			"      \"$ref\": \"14\"" +
+			"    }," +
+			"    {" +
+			"      \"conditions\": [" +
+			"        {" +
+			"          \"$ref\": \"13\"" +
+			"        }" +
+			"      ]," +
+			"      \"$type\": \"Assets.Event_Editor.Event_Scripts.ConditionPipeSystem\"," +
+			"      \"$id\": \"21\"" +
+			"    }" +
+			"  ]," +
+			"  \"_root\": {" +
+			"    \"$ref\": \"21\"" +
+			"  }," +
+			"  \"$type\": \"Assets.Event_Scripts.EventPipeWrapper\"" +
+			"}");
+		EventPipeWrapper wrapper = (EventPipeWrapper)data.Deserialize();
+		rootPipe = wrapper._root;
+		rootPipe.PropogateController(this);
+	}
+}
